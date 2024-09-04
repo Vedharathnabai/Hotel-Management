4ab1fb6d5dfa46d16b37459a8314468c9e90fb18
vedharathnabaie <vedharathnabaie@DESKTOP-U1VBBGU>
2024-09-04 19:11:44 +0530

    Created Login Module and Validate usr email and password

--- HotelManagementSystem/Controllers/AccountController.cs
+++ HotelManagementSystem/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 ﻿using HotelManagementSystem.Data;
 using HotelManagementSystem.Models;
+
 using Microsoft.AspNetCore.Mvc;
 
 namespace HotelManagementSystem.Controllers
@@ -38,5 +39,35 @@ namespace HotelManagementSystem.Controllers
         {
             return View();
         }
+
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: /Account/Login
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var customer = _userRepository.GetCustomerByEmail(model.EmailId);
+
+                if (customer != null && customer.Password == model.Password)
+                {
+                    // Authentication successful, set session or cookie if needed
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Invalid login attempt.");
+                }
+            }
+
+            return View(model);
+        }
+
     }
 }
---
+++ HotelManagementSystem/Models/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelManagementSystem.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string EmailId { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+
+}
